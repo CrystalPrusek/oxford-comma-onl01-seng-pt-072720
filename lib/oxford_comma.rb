3e2a9fe88a0fe93500bcb786471953d0b4e4ad6f
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-07 21:19:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,1 @@
-def oxford_comma(array)
-    if array.length == 2
-        array.join(" and ")
-    elsif array.length > 2
-        array.insert(-2, "and")
-        array[0..-1].join(", ")
-    else
-        array.join
-    end
-end
+
