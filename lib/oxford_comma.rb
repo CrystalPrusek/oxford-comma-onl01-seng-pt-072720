0f0f780c01fa780ee106cd71ace7c475eb06d4d5
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-07 21:15:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
 def oxford_comma(array)
 array=["fiddleheads","okra","kohlrabi"]
-
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array.insert(-2, "and")
+        array[0..-1].join(", ")
+    else
+        array.join
+    end
+end
 end
